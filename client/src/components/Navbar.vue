a9036048039e3890762971fc80e35857b87df7f8
Agiff <ghifarimohammadrahadian@gmail.com>
2023-03-08 19:49:10 +0700

    feat: initialize midtrans

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -8,7 +8,7 @@ import { useGlobalStore } from '../stores/global';
       ...mapWritableState(useGlobalStore, ['isLoggedIn'])
     },
     methods: {
-      ...mapActions(useGlobalStore, ['handleLogout'])
+      ...mapActions(useGlobalStore, ['handleLogout', 'topup'])
     },
     created() {
       if (localStorage.access_token) {
@@ -54,6 +54,10 @@ import { useGlobalStore } from '../stores/global';
             <li class="nav-item d-flex align-items-center my-2" v-if="isLoggedIn">
               <span class="material-symbols-outlined me-2">storefront</span>
               <router-link class="nav-link" to="/shop">Shop</router-link>
+            </li>
+            <li class="nav-item d-flex align-items-center my-2" v-if="isLoggedIn">
+              <span class="material-symbols-outlined me-2">credit_card</span>
+              <a @click.prevent="topup(1000000)" class="nav-link" href="#">Top Up</a>
             </li>
             <li class="nav-item d-flex align-items-center my-2" v-if="isLoggedIn">
               <span class="material-symbols-outlined me-2">logout</span>
